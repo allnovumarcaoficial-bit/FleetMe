ace7e716bebaa6213e06027a1b2ec0d2848d77cf
Dael David <daeldrn@gmail.com>
2025-06-30 13:35:12 -0400

    feat: agregar diseño de Dashboard y layout de inicio de sesión

---
+++ src/components/Layouts/DashboardLayout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Sidebar } from "@/components/Layouts/sidebar";
+import { Header } from "@/components/Layouts/header";
+import { usePathname } from "next/navigation";
+import type { PropsWithChildren } from "react";
+
+export default function DashboardLayout({ children }: PropsWithChildren) {
+  const pathname = usePathname();
+  const isSignInPage = pathname === "/auth/sign-in";
+
+  if (isSignInPage) {
+    return (
+      <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+        {children}
+      </main>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+
+      <div className="w-full bg-gray-2 dark:bg-[#020d1a]">
+        <Header />
+
+        <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
